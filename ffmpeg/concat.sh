4b5bd4dc7e6d6fe353d6527bc437d6f1c3fc11c8
Max Manni <manni.massimiliano@gmail.com>
2017-09-30 16:31:41 +0200

    ffmpeg mp4 concat script fix

--- ffmpeg/concat.sh
+++ ffmpeg/concat.sh
@@ -2,20 +2,20 @@
 #!/bin/bash
 
 folder=$1
-if [ -d "$folder-tmp" ]
-then
-	rm -r "$folder-tmp"
-fi
-mkdir -p "$folder-tmp"
 
+rm -r "$folder"/*.ts
+rm "$folder"/output.mp4
 
 a=0
 s="concat"
 for entry in "$folder"/*
 do
 	intermediateEntry="${entry/.mp4/.ts}"
-	echo "create intermediate file $intermediateEntry"
+	echo "********************************************************"
+	echo "********* create intermediate file $intermediateEntry"
+	echo "********************************************************"
 	ffmpeg -i $entry -c copy -bsf:v h264_mp4toannexb -f mpegts $intermediateEntry
+	
 	if [ "$a" -eq "0" ]
 	then
 		s="$s:$intermediateEntry"
@@ -26,5 +26,7 @@ do
 	let "a += 1"
 done
 
-echo "concatenate $a files: $s"
-ffmpeg -i "$s" -c copy -bsf:a aac_adtstoasc "output-$folder.mp4"
+echo "********************************************************"
+echo "*************** concatenate $a files: $s"
+echo "********************************************************"
+ffmpeg -i "$s" -c copy -bsf:a aac_adtstoasc "$folder"/output.mp4
